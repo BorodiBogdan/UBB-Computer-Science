1078e01614fbda0bf33bd1807aa4e8337ef7a6d8
Borodi Bogdan <bogdanborodiv@gmail.com>
2024-07-05 10:56:43 +0300

    Updated materials
    Uploaded sem2 files

---
+++ Year 1/Sem 2/OOP/lab-work/oop-6-7Database/build/CMakeFiles/a4-oop.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a4-oop.dir/main.cpp.obj"
+  "CMakeFiles/a4-oop.dir/main.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/sqlite-amalgamation-3450300/sqlite3.c.obj"
+  "CMakeFiles/a4-oop.dir/sqlite-amalgamation-3450300/sqlite3.c.obj.d"
+  "CMakeFiles/a4-oop.dir/src/Exceptions/Exceptions.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/Exceptions/Exceptions.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/domain/domain.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/domain/domain.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/repository/CSVRepository.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/repository/CSVRepository.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/repository/DBRepository.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/repository/DBRepository.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/repository/HTMLRepository.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/repository/HTMLRepository.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/repository/repository.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/repository/repository.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/service/service.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/service/service.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/ui/ui.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/ui/ui.cpp.obj.d"
+  "CMakeFiles/a4-oop.dir/src/validators/MovieValidator.cpp.obj"
+  "CMakeFiles/a4-oop.dir/src/validators/MovieValidator.cpp.obj.d"
+  "a4-oop.exe"
+  "a4-oop.exe.manifest"
+  "a4-oop.pdb"
+  "liba4-oop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/a4-oop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Year 1/Sem 2/OOP/lab-work/oop-6-7Database/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Bob/Documents/FMI - YEAR 1/Sem 2/OOP/labs/oop-6-7Database
+# Build directory: C:/Users/Bob/Documents/FMI - YEAR 1/Sem 2/OOP/labs/oop-6-7Database/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
