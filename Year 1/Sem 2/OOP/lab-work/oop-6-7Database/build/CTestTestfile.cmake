85160747da61f5231d3b15401126fa18bb6b5760
Borodi Bogdan <77606868+BorodiBogdan@users.noreply.github.com>
2024-07-05 11:08:05 +0300

    Delete Year 1/Sem 2/OOP/lab-work directory

--- Year 1/Sem 2/OOP/lab-work/oop-6-7Database/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Bob/Documents/FMI - YEAR 1/Sem 2/OOP/labs/oop-6-7Database
-# Build directory: C:/Users/Bob/Documents/FMI - YEAR 1/Sem 2/OOP/labs/oop-6-7Database/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
