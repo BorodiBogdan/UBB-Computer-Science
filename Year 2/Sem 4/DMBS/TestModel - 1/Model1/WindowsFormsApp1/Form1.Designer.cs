84a6499ea390f15061c1b89fad9012b4905cbc66
BorodiBogdan <bogdanborodiv@gmail.com>
2025-06-29 18:00:10 +0300

    update sem4 again

---
+++ Year 2/Sem 4/DMBS/TestModel - 1/Model1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDentalCabinet = new System.Windows.Forms.DataGridView();
+            this.dgvDentists = new System.Windows.Forms.DataGridView();
+            this.saveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDentalCabinet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDentists)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDentalCabinet
+            // 
+            this.dgvDentalCabinet.AllowUserToOrderColumns = true;
+            this.dgvDentalCabinet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDentalCabinet.Location = new System.Drawing.Point(31, 50);
+            this.dgvDentalCabinet.Name = "dgvDentalCabinet";
+            this.dgvDentalCabinet.Size = new System.Drawing.Size(291, 211);
+            this.dgvDentalCabinet.TabIndex = 0;
+            // 
+            // dgvDentists
+            // 
+            this.dgvDentists.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDentists.Location = new System.Drawing.Point(443, 50);
+            this.dgvDentists.Name = "dgvDentists";
+            this.dgvDentists.Size = new System.Drawing.Size(305, 211);
+            this.dgvDentists.TabIndex = 1;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(336, 310);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(98, 44);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.dgvDentists);
+            this.Controls.Add(this.dgvDentalCabinet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDentalCabinet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDentists)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDentalCabinet;
+        private System.Windows.Forms.DataGridView dgvDentists;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ Year 2/Sem 4/DMBS/TestModel - 1/Model1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection conn;
+        SqlDataAdapter daDentalCabinet;
+        SqlDataAdapter daDentist;
+        DataSet dset;
+        //BindingSource bsNeighbourHoods;
+        //BindingSource bsSmartHomes;
+
+        SqlCommandBuilder builder;
+
+        string queryDentalCabinet;
+        string queryDentist;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fillData();
+        }
+
+        void fillData()
+        {
+            conn = new SqlConnection(getConnectionString());
+
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception excep)
+            {
+                Console.WriteLine(excep.ToString());
+            }
+
+
+            queryDentalCabinet = "SELECT * from DentalCabinet";
+            queryDentist = "SELECT * FROM Dentist";
+
+            daDentalCabinet = new SqlDataAdapter(queryDentalCabinet, conn);
+            daDentist = new SqlDataAdapter(queryDentist, conn);
+
+            builder = new SqlCommandBuilder(daDentalCabinet);
+            SqlCommandBuilder cmdBuildersSmartHomes = new SqlCommandBuilder(daDentist);
+
+            dset = new DataSet();
+
+            daDentalCabinet.Fill(dset, "DentalCabinet");
+            daDentist.Fill(dset, "Dentist");
+
+            dset.Relations.Add("DentalDentist",
+               dset.Tables["DentalCabinet"].Columns["DentalCabinetId"],
+               dset.Tables["Dentist"].Columns["DentalCabinetId"] 
+           );
+
+
+            BindingSource bsParent = new BindingSource();
+            bsParent.DataSource = dset.Tables["DentalCabinet"];
+            BindingSource bsChild = new BindingSource(bsParent, "DentalDentist");
+            this.dgvDentalCabinet.DataSource = bsParent;
+            this.dgvDentists.DataSource = bsChild;
+
+            /*
+            this.dgvDentalCabinet.DataSource = dset.Tables["DentalCabinet"];
+
+            this.dgvDentists.DataSource = this.dgvDentalCabinet.DataSource;
+            this.dgvDentists.DataMember = "DentalDentist"; 
+            */
+        }
+        string getConnectionString() {
+            return "Data Source=DESKTOP-26CIK75\\SQLEXPRESS;" +
+                  "Initial Catalog=Dental;Integrated Security=true;";
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dset.HasChanges())
+                {
+                    daDentalCabinet.Update(dset, "DentalCabinet");
+                    daDentist.Update(dset, "Dentist");
+
+                    dset.AcceptChanges();
+                    MessageBox.Show("Updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating database: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
